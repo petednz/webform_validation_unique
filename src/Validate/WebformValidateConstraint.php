9780fcf40c9ae19ca09df07b494c5e25ff82e9ec
Jon Goldberg <jon@megaphonetech.com>
2021-09-10 15:11:14 -0400

    handle placing error on correct element when it has a parent element

--- src/Validate/WebformValidateConstraint.php
+++ src/Validate/WebformValidateConstraint.php
@@ -25,7 +25,7 @@ class WebformValidateConstraint {
     if ($values['properties']['unique_field_values'] ?? FALSE) {
       $uniqueComponents = array_filter($values['properties']['unique_field_value_components']);
       if (empty($uniqueComponents)) {
-        $formState->setErrorByName('equal_components', 'Please select at least 1 Unique components.');
+        $formState->setErrorByName('unique_field_values_components', 'Please select at least 1 Unique components.');
       }
     }
   }
@@ -65,7 +65,7 @@ class WebformValidateConstraint {
   }
 
   /**
-   * Validates Equal components on front end.
+   * Validates unique components on front end.
    *
    * @param array $element
    *   The form element to process.
@@ -96,7 +96,7 @@ class WebformValidateConstraint {
     // Set error messages if needed.
     foreach ($duplicates as $duplicateElementKey => $duplicateValue) {
       // Get the key of the first element with a duplicated value (by diffing the original comparedValues with a deduplicated copy).
-      $errorElement = $form['elements'][$duplicateElementKey];
+      $errorElement = self::recursiveFind($form['elements'], $duplicateElementKey);
       if (isset($errorElement['#title'])) {
         $tArgs = ['%name' => empty($errorElement['#title']) ? $errorElement['#parents'][0] : $errorElement['#title']];
         $formState->setError($errorElement, t('%name is not a unique value', $tArgs));
@@ -107,38 +107,18 @@ class WebformValidateConstraint {
     }
   }
 
-  /**
-   * Check Element Access.
-   *
-   * @param array $elements
-   *   The form elements.
-   * @param string $searchKey
-   *   Key of the equal component.
-   * @param bool $found
-   *   The key.
-   * @param array $element
-   *   Result array.
-   */
-  private static function getFormElementAccess(array &$elements, string $searchKey, bool &$found, array &$element): void {
-    if (!$found) {
-      $element['access'] = $element['multiple'] = FALSE;
-      foreach ($elements as $keyElement => &$keyValue) {
-        if (!WebformElementHelper::isElement($keyValue, $keyElement)) {
-          continue;
-        }
-        if (!empty($keyElement) && $keyElement == $searchKey) {
-          $found = TRUE;
-          $element = $keyValue;
-          if (!empty($keyValue['#access']) || !empty($keyValue['#visited'])) {
-            $element['access'] = TRUE;
-          }
-          if (!empty($keyValue['#webform_multiple'])) {
-            $element['multiple'] = TRUE;
-          }
-        }
-        elseif (!$found) {
-          self::getFormElementAccess($keyValue, $searchKey, $found, $element);
-        }
+  private static function recursiveFind(array $haystack, $needle)  {
+    $iterator  = new \RecursiveArrayIterator($haystack);
+    $recursive = new \RecursiveIteratorIterator(
+      $iterator,
+      \RecursiveIteratorIterator::SELF_FIRST
+    );
+    foreach ($recursive as $key => $value) {
+      if (!is_array($value) || !isset($value['#title'])) {
+        continue;
+      }
+      if ($key === $needle) {
+        return $value;
       }
     }
   }
